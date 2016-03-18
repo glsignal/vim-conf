5901f745210e1bfbaa2700a7936d58322314abf7
Greg Signal <greg@cardboardstraw.com>
2016-03-18 22:15:33 +1300

    Setup script

---
+++ setup.sh
@@ -0,0 +1,16 @@
+if which git &> /dev/null; then
+  echo "Fetching submodules"
+  git submodule update --init
+else
+  echo "Please install git"
+fi
+
+CURRENT_DIR=$(pwd)
+
+echo "Linking $HOME/.vim   -> $CURRENT_DIR"
+ln -s "$CURRENT_DIR" "$HOME/.vim"
+
+echo "Linking $HOME/.vimrc -> $CURRENT_DIR/vimrc"
+ln -s "$CURRENT_DIR/vimrc" "$HOME/.vimrc"
+
+echo "Done!"
