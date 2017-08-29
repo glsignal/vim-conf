4be346ff39f4ec751b277521ee598b5043ece0da
Greg Signal <greg@cardboardstraw.com>
2017-08-29 18:14:50 +1200

    Migrate to vim plug

--- setup.sh
+++ setup.sh
@@ -7,10 +7,13 @@ fi
 
 CURRENT_DIR=$(pwd)
 
-echo "Linking $HOME/.vim   -> $CURRENT_DIR"
+echo "Linking $HOME/.vim          -> $CURRENT_DIR"
 ln -s "$CURRENT_DIR" "$HOME/.vim"
 
-echo "Linking $HOME/.vimrc -> $CURRENT_DIR/vimrc"
+echo "Linking $HOME/.vimrc        -> $CURRENT_DIR/vimrc"
 ln -s "$CURRENT_DIR/vimrc" "$HOME/.vimrc"
+
+echo "Linking $HOME/.vim_plugins  -> $CURRENT_DIR/vim_plugins"
+ln -s "$CURRENT_DIR/vim_plugins" "$HOME/.vim_plugins"
 
 echo "Done!"
